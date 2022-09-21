019af1d60cd02475ab2276306a99f1f399b7d604
Claudia1234567890 <109091065+Claudia1234567890@users.noreply.github.com>
2022-09-21 09:06:48 -0600

    correcciones

--- AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Common/Models/TiposPagosViewModel.cs
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Common/Models/TiposPagosViewModel.cs
@@ -15,8 +15,5 @@ namespace AHM.Total.Travel.Common.Models
         public DateTime? TiPa_FechaModifica { get; set; }
         public bool? TiPa_Estado { get; set; }
 
-        public virtual tbUsuarios TiPa_UsuarioCreacionNavigation { get; set; }
-        public virtual tbUsuarios TiPa_UsuarioModificaNavigation { get; set; }
-        public virtual ICollection<tbRegistrosPagos> tbRegistrosPagos { get; set; }
     }
 }
