c0b6d8326a11f8e70b5d35294c9998b0393b043b
Kiritsugu-sama <109097530+Kiritsugu-sama@users.noreply.github.com>
2022-09-22 09:34:43 -0600

    Esta vez si xd

--- AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Api/Controllers/HabitacionesController.cs
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Api/Controllers/HabitacionesController.cs
@@ -1,11 +1,61 @@
-﻿using System;
+﻿using AHM.Total.Travel.BusinessLogic.Services;
+using AHM.Total.Travel.Common.Models;
+using AHM.Total.Travel.Entities.Entities;
+using AutoMapper;
+using ConciertosProyecto.BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AHM.Total.Travel.Api.Controllers
 {
-    public class HabitacionesController
+    [ApiController]
+    [Route("[controller]")]
+    public class HabitacionesController : Controller
     {
+        private readonly HotelService _hotelService;
+        private readonly IMapper _mapper;
+        public HabitacionesController(HotelService hotelService, IMapper mapper)
+        {
+            _hotelService = hotelService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            var list = _hotelService.ListHabitaciones();
+            return Ok(list);
+
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(int id, HabitacionesViewModel items)
+        {
+
+            var item = _mapper.Map<tbHabitaciones>(items);
+            var result = _hotelService.UpdateHabitaciones(id, item);
+            return Ok(result);
+
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id, int Mod)
+        {
+            var result = _hotelService.DeleteHabitaciones(id, Mod);
+            return Ok(result);
+
+        }
+
+        [HttpGet("Find")]
+        public IActionResult Details(int Id)
+        {
+
+            var result = _hotelService.FindHabitaciones(Id);
+            return Ok(result);
+        }
     }
 }
--- AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Api/Controllers/HotelesActividadesController.cs
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Api/Controllers/HotelesActividadesController.cs
@@ -1,11 +1,62 @@
-﻿using System;
+﻿using AHM.Total.Travel.BusinessLogic.Services;
+using AHM.Total.Travel.Common.Models;
+using AHM.Total.Travel.Entities.Entities;
+using AutoMapper;
+using ConciertosProyecto.BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AHM.Total.Travel.Api.Controllers
 {
-    public class HotelesActividadesController
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelesActividadesController : Controller
     {
+        private readonly HotelService _hotelService;
+        private readonly IMapper _mapper;
+        public HotelesActividadesController(HotelService hotelService, IMapper mapper)
+        {
+            _hotelService = hotelService;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            var list = _hotelService.ListHotelActivities();
+            return Ok(list);
+
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(int id, HotelesActividadesViewModel items)
+        {
+
+            var item = _mapper.Map<tbHotelesActividades>(items);
+            var result = _hotelService.UpdateHotelActivities(id, item);
+            return Ok(result);
+
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id, int Mod)
+        {
+            var result = _hotelService.DeleteHotelActivities(id, Mod);
+            return Ok(result);
+
+        }
+
+        [HttpGet("Find")]
+        public IActionResult Details(int Id)
+        {
+
+            var result = _hotelService.FindHotelActivities(Id);
+            return Ok(result);
+        }
+
     }
 }
