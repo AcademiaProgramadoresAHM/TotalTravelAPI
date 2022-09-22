29cb177ade8175f692a539bcf6aa73ab961a2526
May03 <105520512+MayCruz03@users.noreply.github.com>
2022-09-22 10:05:44 -0600

    2 controllers de 3

---
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Api/Controllers/PartnersController.cs
@@ -0,0 +1,59 @@
+﻿using AHM.Total.Travel.BusinessLogic.Services;
+using AHM.Total.Travel.Common.Models;
+using AHM.Total.Travel.Entities.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM.Total.Travel.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PartnersController : Controller
+    {
+        private readonly GeneralService _generalService;
+        private readonly IMapper _mapper;
+
+        public PartnersController(GeneralService generalService, IMapper mapper)
+        {
+            _generalService = generalService;
+            _mapper = mapper;
+        }
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            var list = _generalService.ListPartners();
+            return Ok(list);
+
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(int id, PartnersViewModel items)
+        {
+
+            var item = _mapper.Map<tbPartners>(items);
+            var result = _generalService.UpdatePartner(id, item);
+            return Ok(result);
+
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id, int Mod)
+        {
+            var result = _generalService.DeletePartner(id, Mod);
+            return Ok(result);
+
+        }
+
+        [HttpGet("Find")]
+        public IActionResult Details(int Id)
+        {
+
+            var result = _generalService.FindPartner(Id);
+            return Ok(result);
+        }
+    }
+}
