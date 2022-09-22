a5004089c237fd4802b7bb6ec63750f73c9f9790
RonaldIsai <87435489+RonaldIsai@users.noreply.github.com>
2022-09-21 21:21:31 -0600

    ViewModels Usuarios, Actividades, ActividadesExtras

---
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Common/Models/ActividadesExtrasViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AHM.Total.Travel.Common.Models
+{
+    public class ActividadesExtrasViewModel
+    {
+        public int ID { get; set; }
+        public string Partner { get; set; }
+        public string Actividad { get; set; }
+        public decimal? Precio { get; set; }
+        public string Descripcion { get; set; }
+        public int? ID_Crea { get; set; }
+        public string UsuarioCrea { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public int? ID_Modifica { get; set; }
+        public string UsuarioModifica { get; set; }
+        public DateTime? FechaModifica { get; set; }
+        public bool? Estado { get; set; }
+    }
+}
---
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Common/Models/ActividadesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AHM.Total.Travel.Common.Models
+{
+    public class ActividadesViewModel
+    {
+        public int ID { get; set; }
+        public string Descripcion { get; set; }
+        public string Tipo { get; set; }
+        public int? ID_Crea { get; set; }
+        public string UsuarioCrea { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public int? ID_Modifca { get; set; }
+        public string UsuarioModifica { get; set; }
+        public DateTime? FechaModifica { get; set; }
+        public bool? Estado { get; set; }
+    }
+}
