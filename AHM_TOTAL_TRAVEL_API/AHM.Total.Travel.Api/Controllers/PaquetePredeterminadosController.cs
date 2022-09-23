e23dcee086c1ae8909a4249f7306ee4bdc0f9921
AngelGuillen02 <109125563+AngelGuillen02@users.noreply.github.com>
2022-09-22 22:57:26 -0600

    Controller de tbPaquetePredeterminados, tbReservacionTransporte, tbReservacionRestaurantes

---
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Api/Controllers/PaquetePredeterminadosController.cs
@@ -0,0 +1,57 @@
+﻿using AHM.Total.Travel.BusinessLogic.Services;
+using AHM.Total.Travel.Common.Models;
+using AHM.Total.Travel.Entities.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM.Total.Travel.Api.Controllers
+{
+    [ApiController]
+    [Route(" [ApiController] ")]
+    public class PaquetePredeterminadosController : Controller
+    {
+        private readonly SaleService _saleService;
+        private readonly IMapper _mapper;
+        public PaquetePredeterminadosController(SaleService saleService, IMapper mapper)
+        {
+            _saleService = saleService;
+            _mapper = mapper;
+        }
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            var list = _saleService.Listpackages();
+            return Ok(list);
+
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(int id, PaquetePredeterminadosViewModel items)
+        {
+
+            var item = _mapper.Map<tbPaquetePredeterminados>(items);
+            var result = _saleService.Updatepackages(id, item);
+            return Ok(result);
+
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id, int Mod)
+        {
+            var result = _saleService.Deletepackages(id, Mod);
+            return Ok(result);
+
+        }
+
+        [HttpGet("Find")]
+        public IActionResult Details(int id)
+        {
+            var result = _saleService.FindPackage(id);
+            return Ok(result);
+        }
+    }
+}
---
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Api/Controllers/ReservacionRestaurantesController.cs
@@ -0,0 +1,57 @@
+﻿using AHM.Total.Travel.BusinessLogic.Services;
+using AHM.Total.Travel.Common.Models;
+using AHM.Total.Travel.Entities.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM.Total.Travel.Api.Controllers
+{
+    [ApiController]
+    [Route(" [ApiController] ")]
+    public class ReservacionRestaurantesController : Controller
+    {
+        private readonly ReservationService  _reservationService;
+        private readonly IMapper _mapper;
+        public ReservacionRestaurantesController(ReservationService  reservationService, IMapper mapper)
+        {
+            _reservationService = reservationService;
+            _mapper = mapper;
+        }
+        [HttpGet("List")]
+        public IActionResult List()
+        {
+            var list = _reservationService.ListReservationRestaurant();
+            return Ok(list);
+
+        }
+
+        [HttpPut("Update")]
+        public IActionResult Update(int id, ReservacionRestaurantesViewModel items)
+        {
+
+            var item = _mapper.Map<tbReservacionRestaurantes>(items);
+            var result = _reservationService.UpdateReservationRestaurant(id, item);
+            return Ok(result);
+
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id, int Mod)
+        {
+            var result = _reservationService.DeleteReservationRestaurant(id, Mod);
+            return Ok(result);
+
+        }
+
+        [HttpGet("Find")]
+        public IActionResult Details(int Id)
+        {
+            var result = _reservationService.FindReservationRestaurant(Id);
+            return Ok(result);
+        }
+    }
+}
