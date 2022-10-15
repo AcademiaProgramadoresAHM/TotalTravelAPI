b9c67c065f7469bf1310995c77afd3c292ef798a
Werundy <angeljmacea@gmail.com>
2022-10-14 18:38:43 -0600

    Actualizacion modelo Addresss

--- AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Common/Models/DireccionesViewModel.cs
+++ AHM_TOTAL_TRAVEL_API/AHM.Total.Travel.Common/Models/DireccionesViewModel.cs
@@ -6,9 +6,11 @@ namespace AHM.Total.Travel.Common.Models
 {
     public class DireccionesViewModel
     {
-        public string Dire_Descripcion { get; set; }
-        public int? Ciud_ID { get; set; }
+        public int? Colo_ID { get; set; }
+        public string Dire_Calle { get; set; }
+        public string Dire_Avenida { get; set; }
         public int? Dire_UsuarioCreacion { get; set; }
         public int? Dire_UsuarioModifica { get; set; }
+     
     }
 }
